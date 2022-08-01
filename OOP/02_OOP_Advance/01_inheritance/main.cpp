12c956027ccfa259ac06fd30050edc4901defa2e
mikami-yua <lovejiaxi_1@163.com>
2022-08-01 15:39:47 +0800

    p5

---
+++ OOP/02_OOP_Advance/01_inheritance/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using std::string;
+
+class Vechicle{
+    public:
+        int wheels = 0;
+        string color="blue";
+        bool airbag = false;
+        void Print() const
+        {
+            std::cout << "This " << color << " vehicle has " << wheels << " wheels!\n";
+        }
+};
+
+class Car: public Vechicle{
+    public:
+        bool sunroof=false;
+};
+
+class Bicyle: public Vechicle{
+    public:
+        bool kickstand=true;
+};
+
+class Truck: public Vechicle{
+    public:
+        int seat=3;
+};
+
+int main(){
+    Car car;
+    car.wheels=4;
+    car.sunroof=true;
+    car.Print();
+    if (car.sunroof)
+    {
+        std::cout<<"And a sunroof!\n";
+    }
+    Vechicle vechicle;
+    vechicle.airbag=true;
+
+    Truck truck;
+    truck.seat=30;
+    std::cout << "My truck has " << truck.seat << " seats"
+              << "\n";
+}
+
+
+
